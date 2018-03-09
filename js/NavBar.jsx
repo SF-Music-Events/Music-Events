d1b0ac48ee250e264885165c544facc991a4688d
augustusmb <augustusmb@gmail.com>
2018-03-08 22:01:56 -0800

    Add basic NavBar

---
+++ js/NavBar.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const NavBar = () => (
+  <div className="navBar">
+    <h1>SFFF Music Events</h1>
+    <span>Upcoming</span>
+    <span>Past</span>
+    <span>Venues</span>
+    <span>Promoters</span>
+  </div>
+);
+
+export default NavBar;

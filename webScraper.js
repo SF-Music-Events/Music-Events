2b65c8697a833951dd9f0292691aad05619570f3
augustusmb <augustusmb@gmail.com>
2018-03-16 16:01:01 -0700

    Scrape and clean music event data

---
+++ webScraper.js
@@ -0,0 +1,59 @@
+const cheerio = require('cheerio');
+const request = require('request');
+
+const url = 'https://19hz.info/eventlisting_BayArea.php';
+
+request(url, (error, response, html) => {
+  if (!error) {
+    const $ = cheerio.load(html);
+
+    const storage = [];
+
+    $('table:first-of-type > tbody > tr').filter(function() {
+      const data = $(this);
+
+      const event = [];
+      const linkList = [];
+
+      data.children().filter(function() {
+        const item = $(this);
+        if (item.children().attr('href')) {
+          linkList.push(item.children().attr('href'));
+        }
+        event.push(item.text());
+      });
+      event.push(linkList);
+      storage.push(event);
+    });
+
+    const cleanedData = storage.map(event => {
+      const cityMarker = event[1].lastIndexOf('(');
+      const venueMarker = event[1].lastIndexOf('@');
+      const priceMarker = event[3].lastIndexOf('|');
+
+      const city = event[1].slice(cityMarker + 1, event[1].length - 1);
+      const title = event[1].slice(0, venueMarker - 1);
+      const venue = event[1].slice(venueMarker + 2, cityMarker - 1);
+      const time = event[0];
+      const date = event[6];
+      const tags = event[2];
+      const price = event[3].slice(0, priceMarker - 1);
+      const age = event[3].slice(priceMarker + 1);
+      const organizers = event[4];
+      const links = event[7];
+
+      return {
+        title,
+        city,
+        venue,
+        time,
+        date,
+        tags,
+        price,
+        age,
+        organizers,
+        links
+      };
+    });
+  }
+});

93a5a8bd9d13db0d720deab4457d655522ba4191
augustusmb <augustusmb@gmail.com>
2018-03-30 19:35:06 -0700

    convert file to class react component, import scraped data and pass to EventList

--- js/App.jsx
+++ js/App.jsx
@@ -1,13 +1,25 @@
 import React from 'react';
-import preload from '../data.json';
 import NavBar from './NavBar';
 import EventList from './EventList';
+import Data from '../scrapedData.json';
 
-const App = () => (
-  <div>
-    <NavBar />
-    <EventList events={preload.events} />
-  </div>
-);
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      events: Data
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <NavBar />
+        <br />
+        <EventList events={Data.events} />
+      </div>
+    );
+  }
+}
 
 export default App;

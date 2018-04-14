535fd2f04f2150e65e4270c7ae43a683c4a2fc2e
augustusmb <augustusmb@gmail.com>
2018-04-14 01:38:41 -0700

    convert FitlerTable to class component with state holding current filters, render FilterLists

--- js/FilterTable.jsx
+++ js/FilterTable.jsx
@@ -1,37 +1,71 @@
 import React from 'react';
+import FilterList from './FilterList';
 
-const FilterTable = () => (
-  <div>
-    <form>
-      <table>
-        <tbody>
-          <tr>
-            <th />
-            <th>Age</th>
-            <th>Price</th>
-            <th>Tags</th>
-          </tr>
-          <tr>
-            <td>
-              <h3>Filters</h3>
-            </td>
-            <td>
-              <span className="filter">18+</span>
-            </td>
-            <td>
-              <span className="filter">Free</span>
-            </td>
-            <td>
-              <span>Dropdown</span>
-            </td>
-            <td>
-              <button>Filter Results</button>
-            </td>
-          </tr>
-        </tbody>
-      </table>
-    </form>
-  </div>
-);
+class FilterTable extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      filters: {
+        city: {},
+        venue: {}
+      }
+    };
+
+    this.filterResults = this.filterResults.bind(this);
+  }
+
+  filterResults(filterVal, filterType) {
+    console.log(this.state.filters);
+    let newFilterState = { ...this.state.filters };
+    if (this.state.filters[filterType][filterVal]) {
+      delete newFilterState[filterType][filterVal];
+      this.setState({
+        filters: { ...newFilterState }
+      });
+    } else {
+      newFilterState[filterType][filterVal] = true;
+      this.setState({
+        filters: { ...newFilterState }
+      });
+    }
+
+    this.props.filterEvents(this.state.filters, filterType);
+  }
+
+  render() {
+    return (
+      <div>
+        <table>
+          <tbody>
+            <tr>
+              <th>City</th>
+              <th>Venue</th>
+              <th>Age</th>
+              <th>Price</th>
+            </tr>
+            <tr>
+              <td>
+                <FilterList filterItems={this.props.cities} type="city" filterFunc={this.filterResults} />
+              </td>
+              <td>
+                <FilterList filterItems={this.props.venues} type="venue" filterFunc={this.filterResults} />
+              </td>
+              <td>
+                <div className="tag" onClick={() => this.filterResults('18+', 'age')}>
+                  18+
+                </div>
+              </td>
+              <td>
+                <div className="tag" onClick={() => this.filterResults('Free', 'price')}>
+                  Free
+                </div>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    );
+  }
+}
 
 export default FilterTable;
